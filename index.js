dcc8b7d1152439cb587d69b30e864ce2cb82d949
FagotPetrova <demidow197845@yandex.ru>
2024-07-02 09:22:33 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,38 +1,46 @@
 // Получаем все слайдеры и текстовые поля
 const sliders = document.querySelectorAll('input[type="range"]');
 const valueFields = document.querySelectorAll('input[type="number"]');
-let c= parseFloat(document.getElementById("value_C").value)
-let H= parseFloat(document.getElementById("value_H").value)
-let O= parseFloat(document.getElementById("value_O").value)
-let N= parseFloat(document.getElementById("value_N").value)
-let Cl= parseFloat(document.getElementById("value_Cl").value)
-let S= parseFloat(document.getElementById("value_S").value)
-let Si= parseFloat(document.getElementById("value_Si").value)
-let dH0= parseFloat(document.getElementById("value_dH0").value)
-let h_c=8.737
-let h_H=2.488
-let h_0=-0.522
-let h_N=-0.494
-let h_f=0.0236
+
+
+let h_c = 8.737
+let h_H = 2.488
+let h_0 = -0.522
+let h_N = -0.494
+let h_f = 0.0236
+
+
+function result() {
+    let c = parseFloat(document.getElementById("value_C").value)
+    let H = parseFloat(document.getElementById("value_H").value)
+    let O = parseFloat(document.getElementById("value_O").value)
+    let N = parseFloat(document.getElementById("value_N").value)
+    let Cl = parseFloat(document.getElementById("value_Cl").value)
+    let S = parseFloat(document.getElementById("value_S").value)
+    let Si = parseFloat(document.getElementById("value_Si").value)
+    let dH0 = parseFloat(document.getElementById("value_dH0").value)
+    let calculatedResult = 100 / (h_c * c + h_H * H + h_0 * O + h_N * N + h_f * dH0 + 1);
+    document.getElementById("value_result").value = calculatedResult;
+
+}
+
 // Функция для синхронизации значения слайдера и текстового поля
 function syncValues(source, target) {
-  target.value = source.value;
+    target.value = source.value;
 }
 
 // Назначаем обработчики событий для слайдеров
 sliders.forEach((slider, index) => {
-  slider.addEventListener('input', () => {
-    syncValues(slider, valueFields[index]);
-    result()
-  });
+    slider.addEventListener('input', () => {
+        syncValues(slider, valueFields[index]);
+        result()
+    });
 });
 
 // Назначаем обработчики событий для текстовых полей
 valueFields.forEach((field, index) => {
-  field.addEventListener('input', () => {
-    syncValues(field, sliders[index]);
-  });
+    field.addEventListener('input', () => {
+        syncValues(field, sliders[index])
+        result();
+    });
 });
-function result(){
-  result=100/(h_c * c + h_H * H + h_0 * O + h_N * N + h_f * dH0 + 1)
-}
